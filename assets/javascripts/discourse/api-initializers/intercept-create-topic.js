32c6e770c9fc26ad15535a9b9f6f67dbcf4ec283
tiandic <2323748675@qq.com>
2026-09-21 17:56:35 +0800

    Fixes an issue in the "Skip" check box in the selector that did not take effect immediately

--- assets/javascripts/discourse/api-initializers/intercept-create-topic.js
+++ assets/javascripts/discourse/api-initializers/intercept-create-topic.js
@@ -4,6 +4,11 @@ import TopicTemplateSelector from "../components/modal/topic-template-selector.g
 export default apiInitializer("1.8.0", (api) => {
   const modal = api.container.lookup("service:modal");
 
+  function skipTemplateSelector() {
+    const v = api.getCurrentUser()?.custom_fields?.open_directly_with_no_template;
+    return v === true || v === "true";
+  };
+
   api.modifyClass(
     "controller:composer",
     (Superclass) =>
@@ -11,9 +16,7 @@ export default apiInitializer("1.8.0", (api) => {
         open(opts) {
           if (
             opts?.action === "createTopic" &&
-            api.getCurrentUser()?.custom_fields
-              ?.open_directly_with_no_template !== "true"
-          ) {
+            !skipTemplateSelector()) {
             modal.show(TopicTemplateSelector, {
               model: {
                 onSelect: (template) => {
